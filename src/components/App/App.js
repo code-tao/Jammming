28985a302d185215c1b8ec97c6154cb4e4f6ab15
Tao. H <hamzatdestiny@gmail.com>
2022-03-20 20:02:59 +0100

    Initialize project using Create React App, Build web interface and rendering functions

---
+++ src/components/App/App.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import './App.css';
+
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+import Spotify from '../../util/Spotify';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playlistName: '',
+      playlistTracks: [/*
+        { name: 'Lenu', artist: 'Buju', album: 'Single', id: '8923' },
+        { name: 'True Love', artist: 'Wizkid', album: 'Made in Lagos', id: '0923' },
+        { name: 'Mood', artist: 'Wizkid', album: 'Made in Lagos', id: '6729' },*/
+      ],
+      searchResults: [/*
+        { name: 'True Love', artist: 'Wizkid', album: 'Made in Lagos', id: '0923' },
+        { name: 'Mood', artist: 'Wizkid', album: 'Made in Lagos', id: '6729' },
+        { name: 'Lenu', artist: 'Buju', album: 'Single', id: '8923' },
+        { name: 'Piece of Me', artist: 'Wizkid', album: 'Made in Lagos', id: '2279' },
+        { name: 'Walkin Blind', artist: 'Patti Smith', album: 'Dead Man Walking', id: '8023' }*/
+      ]
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  addTrack(track) {
+    const found = this.state.playlistTracks.some(existing => existing.id === track.id);
+    if (!found) {
+      const updatedPlaylistTracks = this.state.playlistTracks;
+      updatedPlaylistTracks.push(track);
+      this.setState({
+        playlistTracks: updatedPlaylistTracks
+      });
+    };
+  }
+
+  removeTrack(track) {
+    const updatedPlaylistTracks = this.state.playlistTracks.filter(existing => existing.id !== track.id);
+    this.setState({
+      playlistTracks: updatedPlaylistTracks
+    });
+  }
+
+  updatePlaylistName(newName) {
+    this.setState({
+      playlistName: newName
+    });
+  }
+
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(track => trackURIs.push(track.uri));
+    Spotify.savePlaylist(this.state.playlistName, trackURIs);
+    this.setState({
+      playlistName: 'New Playlist',
+      playlistTracks: []
+    });
+  }
+
+  search(term) {
+    const newSearchResults = Spotify.search(term);
+    this.setState({
+      searchResults: newSearchResults
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar onSearch={this.search} />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist name={this.state.playlistName} tracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Track/Track.js
@@ -0,0 +1,44 @@
+import React from "react";
+import './Track.css';
+
+export default class Track extends React.Component {
+    constructor(props) {
+        super(props);
+        this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
+    }
+
+    addTrack() {
+        this.props.onAdd(this.props.track);
+    }
+
+    removeTrack() {
+        this.props.onRemove(this.props.track);
+    }
+
+    renderAction() {
+        const isRemoval = this.props.isRemoval;
+        if (this.props.isRemoval) {
+            return (
+                <button className="Track-action" onClick={this.removeTrack}>-</button>
+            )
+        } else {
+            return (
+                <button className="Track-action" onClick={this.addTrack}>+</button>
+            )
+        };
+    }
+
+    render() {
+        const track = this.props.track;
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3>{track.name}</h3>
+                    <p>{track.artist} | {track.album}</p>
+                </div>
+                {this.renderAction()}
+            </div>
+        )
+    }
+}
